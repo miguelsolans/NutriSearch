00aea110529f01d7920d6b45a1862fa8eb65a064
Miguel Solans <miguel.solans@itsector.pt>
2025-03-05 20:53:53 +0000

    Remove main storyboard and add constant struct

---
+++ NutriSearch/NutriSearch/NConstants.swift
@@ -0,0 +1,12 @@
+//
+//  NConstants.swift
+//  NutriSearch
+//
+//  Created by Miguel Solans on 05/03/2025.
+//
+
+import Foundation
+
+struct NConstants {
+    static let kBaseEndpoint = "BaseEndpoint";
+}
